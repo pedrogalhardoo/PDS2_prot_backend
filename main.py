23a7aaf26ba79283c0d50000f2cee28038429657
Pedro Galhardo <pedrogerminiani1@gmail.com>
2025-06-26 09:41:00 -0300

    primeiro

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, status, Depends
+# from fastapi.params import Body
+# from pydantic import BaseModel
+import classes
+import model
+from database import engine, get_db
+from sqlalchemy.orm import Session
+
+model.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# class Mensagem(BaseModel):
+#     titulo: str
+#     conteudo: str
+#     publicada: bool = True
+
+@app.get("/")
+def read_root():
+    return {"Hello": "lala"}
+
+# @app.post("/criar")
+# def criar_valores(res: dict = Body(...)):
+#     print(res)
+#     return {"Mensagem": f"lala: {res['lala']} lele: {res['lele']}"}
+
+# @app.post ("/criar")
+# def criar_valores(nova_mensagem: classes.Mensagem):
+#     print(nova_mensagem)
+#     return {"Mensagem": f"Titulo: {nova_mensagem.titulo} Conteudo: {nova_mensagem.conteudo} Publicada: {nova_mensagem.publicada}"}
+
+@app.post("/criar", status_code=status.HTTP_201_CREATED)
+def criar_valores (nova_mensagem: classes.Mensagem, db: Session = Depends (get_db)):
+    mensagem_criada = model.Model_Mensagem(**nova_mensagem.model_dump())
+    db.add(mensagem_criada)
+    db.commit()
+    db.refresh(mensagem_criada)
+    return {"Mensagem": mensagem_criada}
+
+@app.get("/quadrado/{num}")
+def square(num: int):
+    return num ** 2
+
